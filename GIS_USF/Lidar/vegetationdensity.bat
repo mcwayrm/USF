b8f7896f64254c197dbccbf846a195df0b7d5aa0
Ryan McWay <41755358+mcwayrm@users.noreply.github.com>
2019-06-06 12:09:35 +0300

    Create vegetationdensity.bat

---
+++ GIS_USF/Lidar/vegetationdensity.bat
@@ -0,0 +1,1 @@
+C:\Fusion\cover /all /upper:14 C:\Users\GSAL\Documents\LiDAR-McWay\04_Class\Lubrecht\GroundSurface\Lub_Surface.dtm C:\Users\GSAL\Documents\LiDAR-McWay\04_Class\Lubrecht\GroundSurface\cover2_14.dtm 2 15 M M 1 12 2 2 C:\Users\GSAL\Documents\LiDAR-McWay\04_Class\Lubrecht\Lub_FileList.txt
