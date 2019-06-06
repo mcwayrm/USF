71c8ed0b7eba5b0c6a7fb4c304abd32e998168bc
Ryan McWay <41755358+mcwayrm@users.noreply.github.com>
2019-06-06 12:08:06 +0300

    Create lub_cover.bat

---
+++ GIS_USF/Lidar/lub_cover.bat
@@ -0,0 +1,1 @@
+C:\Fusion\cover C:\Users\GSAL\Documents\LiDAR-McWay\04_Class\Lubrecht\GroundSurface\Lub_Surface.dtm C:\Users\GSAL\Documents\LiDAR-McWay\04_Class\Lubrecht\GroundSurface\cover15.dtm 15 15 M M 1 12 2 2 C:\Users\GSAL\Documents\LiDAR-McWay\04_Class\Lubrecht\Lub_FileList.txt
