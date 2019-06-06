1a821631d06fda26f414f01eb479ac44f0910f41
Ryan McWay <41755358+mcwayrm@users.noreply.github.com>
2019-06-06 12:05:11 +0300

    Create canopysurfacemodel.bat

---
+++ GIS_USF/Lidar/canopysurfacemodel.bat
@@ -0,0 +1,1 @@
+C:\fusion\canopymodel C:\Users\GSAL\Documents\LiDAR-McWay\04_Class\Lubrecht\GroundSurface\Lub_CSM.dtm 2 M M 1 12 2 2 C:\Users\GSAL\Documents\LiDAR-McWay\04_Class\Lubrecht\Lub_FileList.txt
