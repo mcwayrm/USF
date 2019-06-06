eb618426c67b1aef17ebfb95d6473695afc2d9d6
Ryan McWay <41755358+mcwayrm@users.noreply.github.com>
2019-06-06 12:08:41 +0300

    Create polyclipdata.bat

---
+++ GIS_USF/Lidar/polyclipdata.bat
@@ -0,0 +1,1 @@
+C:\fusion\polyclipdata /shape:0,1 C:\Users\GSAL\Documents\LiDAR-McWay\02_Class\02_Data\Sewanee\StudyArea\StudyArea_prj.shp C:\Users\GSAL\Documents\LiDAR-McWay\02_Class\02_Data\Sewanee\LASclip2\SewaneeClipped.las C:\Users\GSAL\Documents\LiDAR-McWay\02_Class\02_Data\Sewanee\SewaneeFileList.txt
