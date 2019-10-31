248db39297751a38b12e1a71515aa8cabde09939
Ryan McWay <ryan.mcway@gmail.com>
2019-10-30 22:54:14 -0700

    Create greeting.py

---
+++ Automate_Boring_Stuff/greeting.py
@@ -0,0 +1,17 @@
+## Greeting Function
+
+def hello():
+    print('Howdy!')
+    print('What is your name?')
+    name = input()
+    print('How are you', name, '?')
+    print('What are you up to ?')
+    stuff = input()
+    print(stuff, 'sounds pretty boring...')
+hello()
+
+## Hello Function
+# 
+# def hello_dude(name):
+#     print('Hello', name)
+# hello_dude('Fred')
