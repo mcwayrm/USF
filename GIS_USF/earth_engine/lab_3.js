3d88cfbc39c1abd1027496e9e588c62d4e8edd54
Ryan McWay <ryan.mcway@gmail.com>
2019-11-06 20:44:25 -0800

    Update lab_3.js

--- GIS_USF/earth_engine/lab_3.js
+++ GIS_USF/earth_engine/lab_3.js
@@ -4,6 +4,17 @@
       Ryan McWay
       11/3/2019
 *******************************/
+var point = /* color: #ff0000 */ee.Geometry.MultiPoint(),
+    SFN = ee.FeatureCollection("users/dsaah/USF_GEE_CLASS/LAB2_SF_neighborhoods"),
+    line = /* color: #98ff00 */ee.Geometry.LineString(
+        [[-122.45229222223492, 37.777555364862536],
+         [-122.44864441797466, 37.77779280501006]]),
+    geometry = /* color: #0b4a8b */ee.Geometry.MultiPoint(),
+    ports = ee.FeatureCollection("users/ryan_mcway/port_cities"),
+    VIIRS = ee.ImageCollection("NOAA/VIIRS/DNB/MONTHLY_V1/VCMSLCFG"),
+    DMSP = ee.ImageCollection("NOAA/DMSP-OLS/CALIBRATED_LIGHTS_V4"),
+    viirs_param = {"opacity":1,"bands":["avg_rad"],"min":1.1346594122052194,"max":50.56581116944552,"gamma":1},
+    dmsp_param = {"opacity":1,"bands":["avg_vis"],"min":143.3107421875,"max":1289.7966796875,"gamma":1};
 
 /*******************************
   Part 1: Vector Geometries
@@ -113,7 +124,22 @@ print('Mean elevation', dict);
 // Make a Feature Map for your project 
 
 // 1) Describe your project
+/*
+  I want to look at the change in Night lights in port cities in response to cruise ship activity.
+  Since night lights are a proxy for port activity, this can be seen as a measure of the correlation of cruise ship activity with economic activity/ growth in ports.
+  Still need to add in the cruise ship activity data. 
+*/
 
 // 2) Make Study Area Map
+// This is a global study so I wont need to limit my study area
+// Adding in the port cities
+print(ports);
+Map.addLayer(ports, {}, 'Port Cities', 0);
 
 // 3) Identify and Map key Datasets
+// Key Datasets are DMSP and VIIRS
+// Will need to combine these two datasets to get the time range I want
+print(VIIRS);
+Map.addLayer(VIIRS, viirs_param, 'VIIRS', 0);
+print(DMSP);
+Map.addLayer(DMSP, dmsp_param, 'DMSP', 0);
